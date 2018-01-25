60b22f8100947ded47272c37aa93ac4b1dcc9030
Ethan <35300976+euthnode@users.noreply.github.com>
2018-01-25 14:50:24 -0500

    Add files via upload

--- snake.js
+++ snake.js
@@ -6,50 +6,65 @@ var snakeDirection;
 var snake = {
     x: 240, //middle
     y: 240, //middle
-    width: 20,
-    height: 20
+    size: 20,
 };
 
 //draws the snake
 function drawSnake() {
 
     //makes the snake
-    ctx.fillRect(snake.x, snake.y, snake.width, snake.height);
+    ctx.fillRect(snake.x, snake.y, snake.size, snake.size);
 
     //check if the snake hit the wall
-    if ((snake.x < 0 || snake.x > c.width - snake.width) || (snake.y < 0 || snake.y > c.height - snake.Height)) {
-        //clear screen
-        ctx.clearRect(0, 0, c.width, c.height);
-        snake.x = 240;
-        snake.y = 240;
-        snakeDirection = 0;
-    }
+    gameOver();
 
     //call the function to move the snake
     move();
-
 }
 
 //moves the snake
 function move() {
 
     //move up
-    if (snakeDirection == 1) {
-        snake.y -= 20;
+    if (snakeDirection == "up") {
+        snake.y -= snake.size;
 
     //move down
-    } else if (snakeDirection == -1) {
-        snake.y += 20;
+    } else if (snakeDirection == "down") {
+        snake.y += snake.size;
 
     //move left
-    } else if (snakeDirection == 2) {
-        snake.x -= 20;
+    } else if (snakeDirection == "left") {
+        snake.x -= snake.size;
 
     //move right
-    } else if (snakeDirection == -2) {
-        snake.x += 20;
-
+    } else if (snakeDirection == "right") {
+        snake.x += snake.size;
     }
+}
+
+function gameOver() {
+
+    //checks x axis
+    if (snake.x < 0 || snake.x > c.width - snake.size) {
+        ctx.clearRect(0, 0, c.width, c.height);
+        snake.x = 240;
+        snake.y = 240;
+        snakeDirection = null;
+
+    //checks y axis
+    } else if (snake.y < 0 || snake.y > c.height - snake.size) {
+        ctx.clearRect(0, 0, c.width, c.height);
+        snake.x = 240;
+        snake.y = 240;
+        snakeDirection = null;
+    }
+}
+
+//clears canvas and draws snake
+function draw() {
+    ctx.clearRect(0, 0, c.width, c.height);
+    drawSnake();
 }
 
 //checks for key presses
@@ -57,24 +72,24 @@ document.addEventListener("keydown", function(event) {
     var key = event.keyCode;
 
     //up
-    if ((key == 119 || key == 87 || key == 38) && snakeDirection != -1) {
-        snakeDirection = 1;
+    if ((key == 119 || key == 87 || key == 38) && snakeDirection != "down") {
+        snakeDirection = "up";
 
     //down
-    } else if ((key == 115 || key == 83 || key == 40) && snakeDirection != 1) {
-        snakeDirection = -1;
+    } else if ((key == 115 || key == 83 || key == 40) && snakeDirection != "up") {
+        snakeDirection = "down";
 
     //left
-    } else if ((key == 97 || key == 65 || key == 37) && snakeDirection != -2) {
-        snakeDirection = 2;
+    } else if ((key == 97 || key == 65 || key == 37) && snakeDirection != "right") {
+        snakeDirection = "left";
 
     //right
-    } else if ((key == 100 || key == 68 || key == 39) && snakeDirection != 2) {
-        snakeDirection = -2;
+    } else if ((key == 100 || key == 68 || key == 39) && snakeDirection != "left") {
+        snakeDirection = "right";
     }
 });
 
 //create a timer to run the game
 var init = (function() {
-    var game = setInterval(drawSnake, 100);
+    var game = setInterval(draw, 70);
 }());
